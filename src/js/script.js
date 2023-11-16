ed98c55696f78270e24f2e568c0a3a98bc6c9978
Pashameister <hvgbh47@gmail.com>
2023-11-16 12:53:46 +0300

    all ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,15 @@
  $(document).ready(function(){
     $('.carousel__inner').slick({
-        
         speed: 1200,
-         adaptiveHeight: true, 
+        adaptiveHeight: true, 
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src=" icons/chevron-right-solid.png"></button>',
          responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                 dots: true,
-                  arrows: false,
+                  dots: true,
+                  arrows: false
                 }
             }
         ] 
