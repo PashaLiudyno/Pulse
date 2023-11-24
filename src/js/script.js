8a4a6ef2539dda0d8481b457b26562eb4269d571
Pashameister <hvgbh47@gmail.com>
2023-11-24 13:39:58 +0300

    valideForms

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,35 @@
       });
 
     });
+      
+    function valideForms(form) {
+      $(form).validate({
+        rules:{
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символов")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+        }
+      });
+
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
 });
